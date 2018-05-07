9af8a5f8a800e378746b5d70b1d954048bdda738
Catalin Manea <maneac4@gmail.com>
2018-05-07 22:46:18 +0300

    add get by id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -30,6 +31,25 @@ app.get('/todos', function(req, res){
     res.send({todos: todos});
   }, function(e){
     res.status(400).send(e);
+  });
+});
+
+//GET TODOBY ID
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){//validating ID
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
