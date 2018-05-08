d10b3762a50138863ac4b124d7c39e3468616f70
Catalin Manea <maneac4@gmail.com>
2018-05-08 22:11:25 +0300

    correction 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
-var MONGODB_URI = 'mongodb://<dbuser>:<dbpassword>@ds119060.mlab.com:19060/todoappuri';
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds119060.mlab.com:19060/todoappuri' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
